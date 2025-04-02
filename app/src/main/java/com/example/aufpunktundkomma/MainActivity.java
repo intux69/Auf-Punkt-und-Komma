e92f71e9a3bb564ee387a0444cfacc4118ad7406
intux69 <intux69@gmail.com>
2025-04-02 20:13:53 +0200

    Auf Punkt und Komma Version 1.0

---
+++ app/src/main/java/com/example/aufpunktundkomma/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.aufpunktundkomma;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    String websiteURL = "https://aufpunktundkomma.de/"; // sets web url
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Webview stuff
+        webview = findViewById(R.id.webView);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl(websiteURL);
+        webview.setWebViewClient(new WebViewClientDemo());
+
+    }
+
+    private class WebViewClientDemo extends WebViewClient {
+        @Override
+        //Keep webview in app when clicking links
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+}
